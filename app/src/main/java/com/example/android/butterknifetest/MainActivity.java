10dfffc8c731e15fc004df178ab78e5840db303f
anewman <alexnewman42@gmail.com>
2018-05-01 14:12:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/butterknifetest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.butterknifetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView) //get the binding and inject into variable
+    TextView textView;
+
+    @OnClick(R.id.btn)
+    void fred() {
+        textView.setText("Button is clicked!");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+}
